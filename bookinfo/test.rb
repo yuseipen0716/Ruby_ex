d4e9fe06c56eb85f1380b2a8c5f6dec3f46712ae
yuseipen0716 <yuseipen1@gmail.com>
2022-01-27 22:16:19 +0900

    frame of the list.erb done

--- bookinfo/test.rb
+++ bookinfo/test.rb
@@ -24,30 +24,39 @@ ActiveRecord::Base.establish_connection(config['db']['sample'])
 #     puts '---------------'
 # end
 
-<%# select文の実行結果を1件ずつ取り出して繰り返し処理 %>
-<% (Bookinfo.first.id..Bookinfo.last.id).each do |id| %>
-    <tr>
-    <% (0..Bookinfo.find_by(id: id).attributes.size-1).each do |column| %>
-        <% if Bookinfo.find_by_id(id: id).attributes.keys[column] == 'id' %>
-            <%# valueに選択したidの値の後に処理(.delete, .edit)を結合した文字列を設定 %>
-            <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.delete"%> /></td>
-            <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.edit"%> /></td>
-        <% end %>
-        <td><%= "#{Bookinfo.find_by(id: id).attributes.values[column].to_s}" %></td>
-    <% end %>
-    </tr>
-<% end %>
+Product.all.each do |product|
+    (0..product.attributes.size-1).each do |column|
+        puts "#{product.attributes.keys[column]}: #{product.attributes.values[column]}"
+    end
+    puts '---------------'
+end
 
-<%# select文の実行結果を1件ずつ取り出して繰り返し処理 %>
-<% (Bookinfo.first.id..Bookinfo.last.id).each do |id| %>
-    <tr>
-    <% (0..Bookinfo.find_by(id: id).attributes.size-1).each do |column| %>
-        <% if column == 1 %>
-            <%# valueに選択したidの値の後に処理(.delete, .edit)を結合した文字列を設定 %>
-            <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.delete"%> /></td>
-            <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.edit"%> /></td>
-        <% end %>
-        <td><%= "#{Bookinfo.find_by(id: id).attributes.values[column].to_s}" %></td>
-    <% end %>
-    </tr>
-<% end %>
+# p Product.all
+
+# <%# select文の実行結果を1件ずつ取り出して繰り返し処理 %>
+# <% (Bookinfo.first.id..Bookinfo.last.id).each do |id| %>
+#     <tr>
+#     <% (0..Bookinfo.find_by(id: id).attributes.size-1).each do |column| %>
+#         <% if Bookinfo.find_by_id(id: id).attributes.keys[column] == 'id' %>
+#             <%# valueに選択したidの値の後に処理(.delete, .edit)を結合した文字列を設定 %>
+#             <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.delete"%> /></td>
+#             <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.edit"%> /></td>
+#         <% end %>
+#         <td><%= "#{Bookinfo.find_by(id: id).attributes.values[column].to_s}" %></td>
+#     <% end %>
+#     </tr>
+# <% end %>
+
+# <%# select文の実行結果を1件ずつ取り出して繰り返し処理 %>
+# <% (Bookinfo.first.id..Bookinfo.last.id).each do |id| %>
+#     <tr>
+#     <% (0..Bookinfo.find_by(id: id).attributes.size-1).each do |column| %>
+#         <% if column == 1 %>
+#             <%# valueに選択したidの値の後に処理(.delete, .edit)を結合した文字列を設定 %>
+#             <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.delete"%> /></td>
+#             <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.edit"%> /></td>
+#         <% end %>
+#         <td><%= "#{Bookinfo.find_by(id: id).attributes.values[column].to_s}" %></td>
+#     <% end %>
+#     </tr>
+# <% end %>
