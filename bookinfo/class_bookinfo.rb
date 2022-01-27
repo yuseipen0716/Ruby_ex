b0c696009b4b810f5bd4a2056cd3c7e482f240a1
yuseipen0716 <yuseipen1@gmail.com>
2022-01-27 19:12:35 +0900

    test

---
+++ bookinfo/class_bookinfo.rb
@@ -0,0 +1,5 @@
+# coding:utf-8
+require 'active_record'
+
+class Bookinfo < ActiveRecord::Base
+end
---
+++ bookinfo/test.rb
@@ -0,0 +1,53 @@
+# coding: utf-8
+require 'yaml'
+require 'active_record'
+
+class Product < ActiveRecord::Base
+end
+
+config = YAML.load_file('../../mysql_cn/database.yml')
+ActiveRecord::Base.establish_connection(config['db']['sample'])
+
+# p Product.column_names[0]
+
+# p Product.first.attributes.keys
+# p Product.first.attributes.values
+
+# (0..Product.attributes.size-1).each do |key, value|
+#     puts "#{Product.column_names[column]}: #{Product.find_by(id: id)}"
+# end
+
+# (Product.first.id..Product.last.id).each do |id|
+#     (0..Product.column_names.size-1).each do |column|
+#         puts "#{Product.find_by(id: id).attributes.keys[column]}: #{Product.find_by(id: id).attributes.values[column]}"
+#     end
+#     puts '---------------'
+# end
+
+<%# select文の実行結果を1件ずつ取り出して繰り返し処理 %>
+<% (Bookinfo.first.id..Bookinfo.last.id).each do |id| %>
+    <tr>
+    <% (0..Bookinfo.find_by(id: id).attributes.size-1).each do |column| %>
+        <% if Bookinfo.find_by_id(id: id).attributes.keys[column] == 'id' %>
+            <%# valueに選択したidの値の後に処理(.delete, .edit)を結合した文字列を設定 %>
+            <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.delete"%> /></td>
+            <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.edit"%> /></td>
+        <% end %>
+        <td><%= "#{Bookinfo.find_by(id: id).attributes.values[column].to_s}" %></td>
+    <% end %>
+    </tr>
+<% end %>
+
+<%# select文の実行結果を1件ずつ取り出して繰り返し処理 %>
+<% (Bookinfo.first.id..Bookinfo.last.id).each do |id| %>
+    <tr>
+    <% (0..Bookinfo.find_by(id: id).attributes.size-1).each do |column| %>
+        <% if column == 1 %>
+            <%# valueに選択したidの値の後に処理(.delete, .edit)を結合した文字列を設定 %>
+            <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.delete"%> /></td>
+            <td><input type='radio' name='operation' value=<%="#{Bookinfo.find_by(id: id).id}.edit"%> /></td>
+        <% end %>
+        <td><%= "#{Bookinfo.find_by(id: id).attributes.values[column].to_s}" %></td>
+    <% end %>
+    </tr>
+<% end %>
