f48e535e501002109789565f8d82ba5fff191827
yuseipen0716 <yuseipen1@gmail.com>
2022-01-30 12:24:35 +0900

    add function entry

--- bookinfo/bookinfo_web.rb
+++ bookinfo/bookinfo_web.rb
@@ -1,5 +1,10 @@
 # coding: utf-8
 require 'webrick'
+require 'yaml'
+require 'active_record'
+
+class Bookinfo < ActiveRecord::Base
+end
 
 # サーバーの設定を書いたハッシュを用意する
 # ポートは通常使う80番ではなく、使ってなさそうなポート番号にしておく。
@@ -45,6 +50,37 @@ end
 
 
 # ==========  登録の処理 ========== 
+# 'http://localhost:8099/entry'で呼び出される
+server.mount_proc('/entry') do |req, res|
+    # 本来ならここで入力データに危険や不正がないかチェックするが、演習の見通しのために割愛している
+    p req.query
+    # データベースに接続する
+    config = YAML.load_file('../../mysql_cn/database.yml')
+    ActiveRecord::Base.establish_connection(config['db']['bookinfos'])
+
+    # idが使用されていた場合は登録できないようにする
+    # 入力されたidをid_checkerに格納して、下記のif文でデータベースにそのidが存在するか評価
+    id_checker = req.query['id']
+    if id_checker
+        puts '入力されたidは使用されています。'
+        return
+    else # id_checkerがnilなら登録作業を実施
+        Bookinfo.create(
+            id: req.query['id'],
+            title: req.query['title'],
+            author: req.query['author'],
+            page: req.query['page'],
+            publish_date: req.query['publish_date']
+        );
+    end
+
+    # 処理の結果を表示する
+    # ERBをERBHandlerを経由せずに直接呼び出して利用している
+    template = ERB.new(File.read('entried.erb'))
+    res.body << template.result(binding)
+
+end
+
 
 
 # Ctrl-C割り込みがあった場合にサーバーを停止する処理を登録しておく
